d4c158d47425bc4e023f14291b33561e1eeb7b4b
FoRlit <1154188273@qq.com>
2025-08-11 17:29:12 +0800

    修复:Vue3 中已弃用的样式组合符,改为deep()

--- src/components/MarkdownRenderer.vue
+++ src/components/MarkdownRenderer.vue
@@ -31,7 +31,7 @@ export default {
 </script>
 
 <style scoped>
-.markdown-content >>> h1 {
+.markdown-content :deep(h1) {
   font-size: 2em;
   margin: 0.67em 0;
   color: #2c3e50;
@@ -39,7 +39,7 @@ export default {
   padding-bottom: 0.3em;
 }
 
-.markdown-content >>> h2 {
+.markdown-content :deep(h2) {
   font-size: 1.5em;
   margin: 0.83em 0;
   color: #34495e;
@@ -47,47 +47,47 @@ export default {
   padding-bottom: 0.3em;
 }
 
-.markdown-content >>> h3 {
+.markdown-content :deep(h3) {
   font-size: 1.17em;
   margin: 1em 0;
   color: #3b4a54;
 }
 
-.markdown-content >>> h4 {
+.markdown-content :deep(h4) {
   font-size: 1em;
   margin: 1.33em 0;
   color: #4a5a65;
 }
 
-.markdown-content >>> h5 {
+.markdown-content :deep(h5) {
   font-size: 0.83em;
   margin: 1.67em 0;
   color: #586872;
 }
 
-.markdown-content >>> h6 {
+.markdown-content :deep(h6) {
   font-size: 0.67em;
   margin: 2.33em 0;
   color: #65737e;
 }
 
-.markdown-content >>> strong {
+.markdown-content :deep(strong) {
   color: #34495e;
   font-weight: 600;
 }
 
-.markdown-content >>> ul {
+.markdown-content :deep(ul) {
   padding-left: 1.2em;
   margin: 0.8em 0;
   list-style-type: disc;
 }
 
-.markdown-content >>> li {
+.markdown-content :deep(li) {
   margin: 0.4em 0;
   line-height: 1.6;
 }
 
-.markdown-content >>> code:not(pre code) {
+.markdown-content :deep(code:not(pre code)) {
   background: #f8f9fa;
   padding: 0.2em 0.4em;
   border-radius: 3px;
@@ -95,7 +95,7 @@ export default {
   font-size: 0.9em;
 }
 
-.markdown-content >>> pre {
+.markdown-content :deep(pre) {
   background: #f8f9fa;
   padding: 1em;
   border-radius: 8px;
@@ -104,14 +104,14 @@ export default {
   line-height: 1.5;
 }
 
-.markdown-content >>> pre code {
+.markdown-content :deep(pre code) {
   display: block;
   padding: 0;
   background: transparent;
   font-size: 0.9em;
 }
 
-.markdown-content >>> blockquote {
+.markdown-content :deep(blockquote) {
   border-left: 4px solid #4a90e2;
   padding: 0.5em 1em;
   margin: 1em 0;
